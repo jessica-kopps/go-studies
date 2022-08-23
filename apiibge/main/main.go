fd319ec10e93438deca5a31b9d79b2c8bbc2815f
Jessica Kopps <jessica.kopps@mercadolivre.com>
2022-08-22 22:30:23 -0300

    Update main.go

--- apiibge/main/main.go
+++ apiibge/main/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	name := []string{"Sergio", "Jessica", "Maria", "João", "Amanda"}
+	name := []string{"Jessica", "Maria", "João"}
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 3; i++ {
 		resp, _ := http.Get("https://servicodados.ibge.gov.br/api/v2/censos/nomes/" + name[i])
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Println((string)(body))
